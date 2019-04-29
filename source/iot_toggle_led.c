1a1884c4200d278f0e099d555d4dd38571da9727
Jennifer de Souza <jenniferdesouza88@gmail.com>
2019-04-28 23:47:36 -0400

    submitted code for blackboard

--- source/iot_toggle_led.c
+++ source/iot_toggle_led.c
@@ -1,7 +1,46 @@
 
+
 #include "iot_toggle_led.h"
 
-void iot_toggle_frdm_led(void)
+#define IOT_TOGGLE_LED_LOG "IOT TOGGLE LED: "
+
+void iot_toggle_frdm_led(char* json)
 {
-	printf("testing out my module, should print after every button press");
+	//printf("JSON in toggle function: %s %d\n", json, strlen(json));
+
+	/*printf("Printing each char: ");
+	for(int i = 0; i < strlen(json); i++)
+	{
+		printf("%c[%d]", json[i], i);
+
+	}*/
+
+	char clickType[6];
+	memcpy(&clickType[0], &json[79], 1);  //[79,83]
+	memcpy(&clickType[1], &json[80], 1);  //[79,83]
+	memcpy(&clickType[2], &json[81], 1);  //[79,83]
+	memcpy(&clickType[3], &json[82], 1);  //[79,83]
+	memcpy(&clickType[4], &json[83], 1);  //[79,83]
+	memcpy(&clickType[5], &json[84], 1);  //[79,83]
+
+	if(!strncmp("DOUBLE", clickType,6)) // clickType is "DOUBLE"
+	{
+		//printf("%s Click type is double\n\tGREEN TOGGLE", IOT_TOGGLE_LED_LOG);
+		LED_RED_OFF();
+		LED_GREEN_TOGGLE();
+	}
+	else if(!strncmp("SINGLE", clickType,6))
+	{
+		//printf("%s Click type is single\n\tRED TOGGLE", IOT_TOGGLE_LED_LOG);
+		//LED_RED_TOGGLE();
+		LED_GREEN_OFF();
+		LED_RED_TOGGLE();
+	}
+	else
+	{
+	    printf("%s Click type not recognized\n", IOT_TOGGLE_LED_LOG);
+	    printf("%s %s", IOT_TOGGLE_LED_LOG, clickType);
+	}
+
+	printf("\n");
 }
--- source/iot_toggle_led.h
+++ source/iot_toggle_led.h
@@ -1,9 +1,13 @@
 #ifndef __IOT_TOGGLE_LED_H__
 #define __IOT_TOGGLE_LED_H__
 
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 #include "board.h"
+#include "fsl_debug_console.h"
 
 /* Public Function Prototype */
-void iot_toggle_frdm_led(void);
+void iot_toggle_frdm_led(char* json);
 
 #endif /* __IOT_TOGGLE_LED_H__ */
